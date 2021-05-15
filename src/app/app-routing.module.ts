7d1363a74d22d78f8f8f69ad3ca03a82b545ab6f
Joshua French <11663405+jfrench7919@users.noreply.github.com>
2021-05-15 11:01:43 -0400

    Added nav bar in to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibCompanyLibComponent } from '@lokidev/company-lib';
+import { LibPersonLibComponent } from '@lokidev/person-lib';
+import { LibTechLibComponent } from '@lokidev/tech-lib';
 import { MongoComponent } from './components/mongo/mongo.component';
 import { ReadMeComponent } from './components/read-me/read-me.component';
 import { MyStoreComponent } from './components/store/store.component';
 
 const routes: Routes = [
+  { path: 'tech', component: LibTechLibComponent },
+  { path: 'companies', component: LibCompanyLibComponent },
+  { path: 'people', component: LibPersonLibComponent },
   { path: 'mongo', component: MongoComponent },
   { path: 'readme', component: ReadMeComponent },
   { path: 'store', component: MyStoreComponent }
